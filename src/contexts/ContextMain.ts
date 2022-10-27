da6678926be9a2ccac91eff5924dd77fdbbaefa3
Sam Boylett <sam.boylett@optix.co.uk>
2022-10-27 21:19:59 +0100

    Add error to field

--- src/contexts/ContextMain.ts
+++ src/contexts/ContextMain.ts
@@ -1,13 +1,15 @@
 import { createContext } from "react";
 import { ContextValuesInterface, defaultContextValues } from "./ContextValues";
 import { EventEmitter } from 'events';
+import { ContextErrorsInterface, defaultContextErrors } from "./ContextErrors";
 
-export interface ContextMainInterface<Values> extends ContextValuesInterface<Values> {
+export interface ContextMainInterface<Values> extends ContextValuesInterface<Values>, ContextErrorsInterface<Values> {
   events: EventEmitter;
 }
 
 export const createContextMain = <Values>() => createContext<ContextMainInterface<Values>>({
   ...defaultContextValues,
+  ...defaultContextErrors,
 
   events: new EventEmitter(),
 });
--- src/hooks/useField.ts
+++ src/hooks/useField.ts
@@ -3,13 +3,16 @@ import { O } from 'ts-toolbelt';
 import { DeepIndex } from '../types/DeepIndex';
 import { UseMainContextReturn } from './useMainContext';
 import get from 'lodash.get';
-import { EVENT_VALUES_CHANGE } from '../constants/events';
+import { EVENT_ERRORS_CHANGE, EVENT_VALUES_CHANGE } from '../constants/events';
+import { FormErrors } from '../types/FormErrors';
 
 export interface UseFieldArg<Name> {
   name: Name;
 }
 
-export interface UseFieldReturn<Values> {
+export interface UseFieldReturn<Values, Name extends string> {
+  value: DeepIndex<Values, Name>;
+  error: string | undefined;
 }
 
 export interface CreateUseFieldDependencies<Values> {
@@ -17,9 +20,10 @@ export interface CreateUseFieldDependencies<Values> {
 }
 
 export const createUseField = <Values>({ useMainContext }: CreateUseFieldDependencies<Values>) => {
-  const useField = <Name extends string & O.Paths<Values>>({ name }: UseFieldArg<Name>): UseFieldReturn<Values> => {
-    const { events, values } = useMainContext();
+  const useField = <Name extends string & O.Paths<Values>>({ name }: UseFieldArg<Name>): UseFieldReturn<Values, Name> => {
+    const { events, values, errors } = useMainContext();
     const [value, setValue] = useState<DeepIndex<Values, Name>>(get(values, name));
+    const [error, setError] = useState<string | undefined>(get(errors, name));
 
     useEffect(() => {
       const handleChange = (newValues: Values) => {
@@ -30,15 +34,26 @@ export const createUseField = <Values>({ useMainContext }: CreateUseFieldDepende
         }
       };
 
+      const handleError = (newErrors: FormErrors<Values>) => {
+        const newError = get(newErrors, name);
+
+        if (error !== newError) {
+          setError(newError);
+        }
+      };
+
       events.on(EVENT_VALUES_CHANGE, handleChange);
+      events.on(EVENT_ERRORS_CHANGE, handleError);
 
       return () => {
         events.off(EVENT_VALUES_CHANGE, handleChange);
+        events.off(EVENT_ERRORS_CHANGE, handleError);
       }
     }, [events, values, name, setValue]);
 
     return {
       value,
+      error,
     };
   };
 
