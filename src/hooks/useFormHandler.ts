fc3350507b89e7706c32db487342987d9b45c97a
Sam Boylett <sam.boylett@optix.co.uk>
2022-10-27 22:01:08 +0100

    Add validation

--- src/hooks/useFormHandler.ts
+++ src/hooks/useFormHandler.ts
@@ -4,43 +4,57 @@ import { FormErrors } from "../types/FormErrors";
 import { EVENT_ERRORS_CHANGE, EVENT_VALUES_CHANGE } from "../constants/events";
 import { O } from 'ts-toolbelt';
 import { DeepIndex } from "../types/DeepIndex";
-import { merge, set } from 'lodash';
+import { merge, set, isEqual } from 'lodash';
 
 export interface UseFormHandlerArg<Values> {
   initialValues: Values;
+  validate?: (values: Values, fieldName?: O.Paths<Values>) => Promise<FormErrors<Values>>;
 }
 
 export interface UseFormHandlerReturn<Values> {
   values: Values;
-  setValues: (values: Values) => void;
+  setValues: (values: Values, shouldValidate?: boolean) => void;
   errors: any;
   setErrors: (errors: any) => void;
   events: EventEmitter;
-  setFieldValue: <Name extends string & O.Paths<Values>>(name: Name, value: DeepIndex<Values, Name>) => void;
+  setFieldValue: <Name extends string & O.Paths<Values>>(name: Name, value: DeepIndex<Values, Name>, shouldValidate?: boolean) => void;
   setFieldError: <Name extends string & O.Paths<Values>>(name: Name, error: string | undefined) => void;
   handleChangeEvent: (event: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => void;
 }
 
 export const createUseFormHandler = <Values>() => {
-  const useFormHandler = ({ initialValues }: UseFormHandlerArg<Values>): UseFormHandlerReturn<Values> => {
+  const useFormHandler = ({ initialValues, validate }: UseFormHandlerArg<Values>): UseFormHandlerReturn<Values> => {
     const events = useMemo(() => new EventEmitter(), []);
     const [values, baseSetValues] = useState<Values>(initialValues);
     const [errors, baseSetErrors] = useState<FormErrors<Values>>({});
 
-    const setValues = useCallback((newValues: Values) => {
-      baseSetValues(newValues);
-
-      events.emit(EVENT_VALUES_CHANGE, newValues);
-    }, [baseSetValues, events]);
-
     const setErrors = useCallback((newErrors: FormErrors<Values>) => {
+      if (isEqual(newErrors, errors)) return;
+
       baseSetErrors(newErrors);
 
       events.emit(EVENT_ERRORS_CHANGE, newErrors);
     }, [baseSetErrors, events]);
 
-    const setFieldValue = useCallback(<Name extends string & O.Paths<Values>>(name: Name, value: DeepIndex<Values, Name>) => {
-      setValues(merge({}, values, set({}, name, value)));
+    const setValues = useCallback(async (newValues: Values, shouldValidate?: boolean) => {
+      if (isEqual(newValues, values)) return;
+
+      baseSetValues(newValues);
+
+      if (shouldValidate && validate) {
+        setErrors(await validate(newValues));
+      }
+
+      events.emit(EVENT_VALUES_CHANGE, newValues);
+    }, [baseSetValues, events]);
+
+    const setFieldValue = useCallback(async <Name extends string & O.Paths<Values>>(name: Name, value: DeepIndex<Values, Name>, shouldValidate?: boolean) => {
+      const newValues = merge({}, values, set({}, name, value));
+      setValues(newValues);
+
+      if (shouldValidate && validate) {
+        setErrors(await validate(newValues, name))
+      }
     }, [setValues, values]);
 
     const setFieldError = useCallback(<Name extends string & O.Paths<Values>>(name: Name, error: string | undefined) => {
