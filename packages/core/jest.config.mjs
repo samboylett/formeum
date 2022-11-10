fd6b73ac53e1f90feeb38be6bcdcd06497b16237
Sam Boylett <sam@boylett.com>
2022-11-10 21:37:04 +0000

    Add coverage

--- packages/core/jest.config.mjs
+++ packages/core/jest.config.mjs
@@ -1,4 +1,9 @@
 import config from "../../jest.config.mjs";
 
-export default config;
+export default {
+    ...config,
+    collectCoverage: true,
+    collectCoverageFrom: ["./src/lib/**/*"],
+    coverageReporters: ["lcov"],
+};
   
