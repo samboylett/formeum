bb60902a8f80c4ebe29b116633457688d48c7c61
Sam Boylett <sam.boylett@optix.co.uk>
2022-11-15 20:15:21 +0000

    useFormCallbacks spec

---
+++ packages/core/src/spec/hooks/useFormCallbacks.spec.tsx
@@ -0,0 +1,41 @@
+import { renderHook, RenderHookResult } from "@testing-library/react";
+import { FORM_CALLBACK_NAMES, UseFormCallbacksReturn } from "../../lib";
+import {
+  TestForm,
+  TestFormValues,
+  createTestProvider,
+  TestProviderHandler,
+} from "../TestForm";
+
+describe("useFormCallbacks", () => {
+  test("is a function", () => {
+    expect(TestForm.useFormCallbacks).toEqual(expect.any(Function));
+  });
+
+  describe("when rendered", () => {
+    let hook: RenderHookResult<
+      UseFormCallbacksReturn<TestFormValues>,
+      void
+    >;
+    let provider: TestProviderHandler;
+
+    beforeEach(() => {
+      provider = createTestProvider();
+
+      hook = renderHook<
+        UseFormCallbacksReturn<TestFormValues>,
+        void
+      >(TestForm.useFormCallbacks, {
+        wrapper: ({ children }) => (
+          <provider.TestProvider>{children}</provider.TestProvider>
+        ),
+      });
+    });
+
+    test.each(FORM_CALLBACK_NAMES)("returns the %s function", (fnName) => {
+      expect(hook.result.current).toEqual(expect.objectContaining({
+        [fnName]: expect.any(Function),
+      }));
+    });
+  });
+});
