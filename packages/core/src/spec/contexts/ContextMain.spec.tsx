cc7a9d50ee4f0d157aabe306b59a6bdb12f16598
Sam Boylett <sam.boylett@optix.co.uk>
2022-11-17 23:33:57 +0000

    Context default spec

---
+++ packages/core/src/spec/contexts/ContextMain.spec.tsx
@@ -0,0 +1,22 @@
+import { renderHook, RenderHookResult } from "@testing-library/react";
+import { useFastContext } from "react-fast-context";
+import { ContextMainInterface } from "../../lib";
+import { NoContextError } from "../../lib/errors/NoContextError";
+import { TestForm, TestFormValues } from "../TestForm";
+
+describe("ContextMain defaults", () => {
+  let hook: RenderHookResult<ContextMainInterface<TestFormValues>, void>;
+
+  beforeEach(() => {
+    hook = renderHook(() => useFastContext(TestForm.ContextMain, () => true));
+  });
+
+  test.each(["values", "initialValues"] as const)(
+    "reading %s throws NoContextError",
+    (attr) => {
+      expect(() => {
+        hook.result.current[attr];
+      }).toThrow(NoContextError);
+    }
+  );
+});
