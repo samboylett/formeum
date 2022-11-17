623bd8698118065a1a243642e0f87c9426cd0698
Sam Boylett <sam.boylett@optix.co.uk>
2022-11-17 22:59:51 +0000

    Add useChangeHandler tests

---
+++ packages/core/src/spec/hooks/useChangeHandler.spec.tsx
@@ -0,0 +1,79 @@
+import { renderHook, RenderHookResult } from "@testing-library/react";
+import { UseChangeHandlerArg, UseChangeHandlerReturn } from "../../lib";
+import { TestForm, createTestProvider, TestProviderHandler, TestFormValues } from "../TestForm";
+
+describe("useChangeHandler", () => {
+  test("is a function", () => {
+    expect(TestForm.useChangeHandler).toEqual(expect.any(Function));
+  });
+
+  describe("when rendered", () => {
+    let hook: RenderHookResult<UseChangeHandlerReturn<TestFormValues, "stringField">, UseChangeHandlerArg<"stringField">>;
+    let provider: TestProviderHandler;
+
+    beforeEach(() => {
+      provider = createTestProvider();
+
+      hook = renderHook<UseChangeHandlerReturn<TestFormValues, "stringField">, UseChangeHandlerArg<"stringField">>(
+        TestForm.useChangeHandler,
+        {
+          initialProps: {
+            name: "stringField",
+          },
+          wrapper: ({ children }) => (
+            <provider.TestProvider>{children}</provider.TestProvider>
+          ),
+        }
+      );
+    });
+
+    describe("when changeValue called", () => {
+      beforeEach(() => {
+        hook.result.current.changeValue("test");
+      });
+
+      test("calls setFieldValue with field name and new value", () => {
+        expect(provider.mocks.setFieldValue).toHaveBeenCalledWith(
+          "stringField",
+          "test"
+        );
+      });
+    });
+
+    describe.each(["target", "currentTarget"] as const)("when event has %s filled", (targetName) => {
+      describe("when handleChangeEvent called", () => {
+        beforeEach(() => {
+          hook.result.current.handleChangeEvent({
+            [targetName]: {
+              value: "foo",
+            }
+          } as any);
+        });
+
+        test("calls setFieldValue with field name and new value", () => {
+          expect(provider.mocks.setFieldValue).toHaveBeenCalledWith(
+            "stringField",
+            "foo"
+          );
+        });
+      });
+
+      describe.each([true, false])("when handleCheckboxEvent called with checked as %j", (checked) => {
+        beforeEach(() => {
+          (hook.result.current.handleCheckboxEvent as any)({
+            [targetName]: {
+              checked,
+            }
+          } as any);
+        });
+
+        test("calls setFieldValue with field name and new value", () => {
+          expect(provider.mocks.setFieldValue).toHaveBeenCalledWith(
+            "stringField",
+            checked
+          );
+        });
+      });
+    });
+  });
+});
