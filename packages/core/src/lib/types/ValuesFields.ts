3509825372e5b697a39ff33413e02de9ec29e387
Sam Boylett <sam@boylett.com>
2023-07-28 08:38:12 +0100

    Fix types

--- packages/core/src/lib/types/ValuesFields.ts
+++ packages/core/src/lib/types/ValuesFields.ts
@@ -1,8 +1,12 @@
-import { L, O, S } from "ts-toolbelt";
+export type Keyable = string | number | bigint | boolean | null | undefined;
 
-export type ValuesPaths<Values> = L.Required<O.Paths<Values>>;
+export type GetChildKeys<
+  Data extends Record<any, any>,
+  Key extends keyof Data
+> = Data[Key] extends Record<any, any>
+  ? ValuesFields<Data[Key]>
+  : never;
 
-export type ValuesFields<Values> =
-  ValuesPaths<Values> extends ReadonlyArray<string>
-    ? S.Join<ValuesPaths<Values>, ".">
-    : never;
+export type ValuesFields<Data extends Record<any, any>> = keyof Data | {
+  [Key in keyof Data]: Key extends Keyable ? GetChildKeys<Data, Key> extends Keyable ? `${Key}.${GetChildKeys<Data, Key>}` : never : never;
+}[keyof Data];
